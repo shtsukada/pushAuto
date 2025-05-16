8bf4bb28b9c36ef5fdbe91156cd5a96799f338ec
shtsukada <st01251988@gmail.com>
2025-05-16 23:05:57 +0900

    api

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module pushAuto
 
 go 1.24.2
 
-require github.com/fatih/color v1.18.0
+require (
+	github.com/fatih/color v1.18.0
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- init/init.go
+++ init/init.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 
 	"github.com/fatih/color"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(".envの読み込みに失敗しました。")
+	}
+
 	//サブコマンド判定
 	if len(os.Args) < 2 || os.Args[1] != "init" {
 		color.Yellow("Usage: pushAuto init -r <repo_url>")
@@ -19,6 +30,7 @@ func main() {
 	//initコマンド用のフラグ定義
 	initCmd := flag.NewFlagSet("init", flag.ExitOnError)
 	repo := initCmd.String("r", "", "GitHub repository URL (e.g., https://github.com/user/repo.git)")
+	private := initCmd.Bool("private", false, "private")
 	initCmd.Parse(os.Args[2:])
 
 	if *repo == "" {
@@ -26,19 +38,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	//既にリポジトリである場合は警告
-	if _, err := os.Stat(".git"); err == nil {
-		color.Yellow("既にこのディレクトリはGitリポジトリです。")
+	cloneURL, err := createGitHubRepo(*repo, *private)
+	if err != nil {
+		color.Red("Githubリポジトリ作成失敗:%v", err)
 		os.Exit(1)
 	}
+	color.Green("Githubリポジトリ作成成功:%s", cloneURL)
 
 	//コマンド実行
 	run("git", "init")
-	run("git", "remote", "add", "origin", *repo)
+	run("git", "remote", "add", "origin", cloneURL)
 	run("git", "add", ".")
 	run("git", "commit", "-m", "init")
 	run("git", "branch", "-M", "main")
 	run("git", "push", "-u", "origin", "main")
+
+	color.Green("Githubリポジトリ初期化、push完了")
+
 }
 
 func run(name string, args ...string) {
@@ -50,4 +66,51 @@ func run(name string, args ...string) {
 		fmt.Fprintf(os.Stderr, "コマンド実行エラー: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+type RepoResponse struct {
+	CloneURL string `json:"clone_url"`
+	HTMLURL  string `json:"html_url"`
+	Name     string `json:"name"`
+}
+
+func createGitHubRepo(repoName string, private bool) (string, error) {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return "", errors.New("環境変数 GITHUB_TOKENが設定されていません。")
+	}
+
+	body := map[string]interface{}{
+		"name":    repoName,
+		"private": private,
+	}
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest("POST", "https://github.com/shtsukada/repos", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		return "", fmt.Errorf("Github API Error:%s", resp.Status)
+	}
+
+	var result RepoResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return "", err
+	}
+	return result.CloneURL, nil
 }
